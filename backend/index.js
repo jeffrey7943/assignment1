125581dedf878329eb3d3797ac3e25cc48399a8d
jeffrey mukkath <jeffreymukkath65@gmail.com>
2022-08-28 12:30:29 +0530

    PREPARING FOR DEPLOYMENT

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const driverRoute = require("./routes/driverRoute");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -10,8 +11,16 @@ const PORT = process.env.PORT;
 
 app.use("/api/drivers", driverRoute);
 
-app.get("/", (req, res) => {
-  res.send("API IS RUNNING");
-});
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
+}
 
 app.listen(PORT, console.log(`SERVER IS RUNNING AT ${PORT}`));
